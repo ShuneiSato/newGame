09df5203e508b2664f757cf80c25907bab498e2f
shun_a123 <sshuna123@gmail.com>
2022-07-12 20:38:49 +0900

    提出用更新

---
+++ Assets/Sclipt/ItemBase2D.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class ItemBase2D : MonoBehaviour
+{
+    [Header("プレイヤーの判定")] public PlayerTriggerCheck playerCheck;
+    [Header("取得時のSE")] public AudioClip getSE;
+
+    public abstract void Activate();
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Player"))
+        {
+
+            if (getSE)
+            {
+                AudioSource.PlayClipAtPoint(getSE, Camera.main.transform.position);
+            }
+
+            Activate();
+            Destroy(this.gameObject);
+        }
+    }
+}
